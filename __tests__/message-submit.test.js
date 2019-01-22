0d7fbd00d6263f8217ca860acd3569be320b4dca
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2019-01-22 09:46:27 +0100

    Basic version of the message-submit tes

---
+++ __tests__/message-submit.test.js
@@ -0,0 +1,7 @@
+
+describe('Message Submit', () => {
+  // store create with the spy
+  // Render the MessageInput with the store
+  // call the action on the MessageInput
+  // expect that the method emit is called on the spy with the correct arguments
+});
